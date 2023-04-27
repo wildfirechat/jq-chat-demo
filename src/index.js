a9c1b09dea053b613a99b6167ea2455dd2d6372d
imndx <imndxx@gmail.com>
2023-04-27 21:56:15 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import EventType from "./wfc/client/wfcEvent";
+
 let baseUrl = 'https://app.wildfirechat.net';
 import wfc from './wfc/client/wfc'
 import Config from './config'
+import TextMessageContent from "./wfc/messages/textMessageContent";
+import Conversation from "./wfc/model/conversation";
+import ConversationType from "./wfc/model/conversationType";
+import {stringValue} from "./wfc/util/longUtil";
 
 wfc.init();
+wfc.eventEmitter.on(EventType.ReceiveMessage, (msg, hasMore) => {
+    $("#console").text("收到消息" + JSON.stringify(msg));
+})
+
 $("#send-code-button").click(function (event) {
     event.preventDefault();
     var phone = $("#phone").val();
@@ -54,6 +64,8 @@ $("#login-form").submit(function (event) {
             //window.location.href = "/home";
             const {userId, token, portrait} = response.result;
             wfc.connect(userId, token);
+            $('#login-form').hide()
+            $('#action-container').show();
             // setItem('userId', userId);
             // setItem('token', token);
             // setItem("userPortrait", portrait);
@@ -62,4 +74,23 @@ $("#login-form").submit(function (event) {
             alert("登录失败：" + error);
         }
     });
+});
+
+$("#get-user-info-button").click(function () {
+    let userInfo = wfc.getUserInfo(wfc.getUserId());
+    console.log('getUserInfo', userInfo)
+    $("#console").text("用户信息" + JSON.stringify(userInfo));
+});
+$("#send-msg-button").click(function () {
+    let textMessageContent = new TextMessageContent('hello world');
+    let conversation = new Conversation(ConversationType.Single, "FireRobot", 0);
+    wfc.sendConversationMessage(conversation, textMessageContent, [], null, null, (messageUid) => {
+        $("#console").text("消息发送成功" + stringValue(messageUid));
+    }, (err) => {
+
+    })
+});
+$("#get-conversation-list-button").click(function () {
+    let conversationList = wfc.getConversationList([0, 1, 2], [0, 1])
+    $("#console").text("会话列表" +  JSON.stringify(conversationList));
 });
